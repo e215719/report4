c43071685d1527a6634384b1da66c19f85541ddd
Ryuki IKEHARA <e215719@ie.u-ryukyu.ac.jp>
2021-12-02 15:17:08 +0900

    ステップ2 Warriorを実装

--- app/src/test/java/jp/ac/uryukyu/ie/e215719/WarriorTest.java
+++ app/src/test/java/jp/ac/uryukyu/ie/e215719/WarriorTest.java
@@ -8,18 +8,19 @@ class WarriorTest {
      * attackWithWeponSkillを実行し、attackの150%ダメージになっていることを確認。
      * 検証手順
      *  (1) ウォーリアと敵を準備。ウォーリアの攻撃力は100とする。
-     *  (2) 敵にウォーリアが150ダメージを与えることを期待。
+     *  (2) 敵にウォーリアが100*1.5のダメージを与えることを期待。
      *  (3) ウォーリアが与えたダメージは150になるはず。
-     * 　　つまりウォーリアが与えたダメージと150が等しいことを期待。これを3回検証する。
+     * 　　つまりウォーリアが与えたダメージとウォーリアの攻撃力*1.5が等しいことを期待。これを3回検証する。
      */
     @Test
     void attackTest() {
         int defaultWarriorAttack = 100;
         Warrior demoWarrior = new Warrior("デモ勇者", 500, defaultWarriorAttack);
         Enemy slime = new Enemy("スライムもどき", 500, 100);
-        for (int i=1; i<3; i++) {
-        demoWarrior.attackWithWeponSkill(slime);
-        assertEquals(defaultWarriorAttack*1.5, 150);
+        for (int i=0; i<3; i++) {
+            int formerEnemyHp = slime.hitPoint;
+            demoWarrior.attackWithWeponSkill(slime);
+            assertEquals(defaultWarriorAttack*1.5, formerEnemyHp-slime.hitPoint);
         }
     }
 }
