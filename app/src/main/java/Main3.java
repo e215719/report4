149151c6410cb13cc3971e14615e1af2acdaab05
Ryuki IKEHARA <e215719@ie.u-ryukyu.ac.jp>
2021-12-06 22:59:27 +0900

    オプション

---
+++ app/src/main/java/Main3.java
@@ -0,0 +1,21 @@
+import jp.ac.uryukyu.ie.e215719.*;
+
+public class Main3 {
+    public static void main(String[] args){
+        Hero hero = new Hero("勇者", 10, 5);
+        Enemy enemy = new Enemy("スライム", 6, 3);
+
+        System.out.println(hero);
+
+        System.out.printf("%s vs. %s\n", hero.getName(), enemy.getName());
+
+        int turn = 0;
+        while( hero.isDead() == false && enemy.isDead() == false ){
+            turn++;
+            System.out.printf("%dターン目開始！\n", turn);
+            hero.attack(enemy);
+            enemy.attack(hero);
+        }
+        System.out.println("戦闘終了");
+    }
+}
